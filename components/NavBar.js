a3a430885558048a410f4a413c21e11e2c9f0fb6
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-05-26 23:03:02 -0300

    first commit

---
+++ components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className=" bg-gray-800 text-white flex justify-between">
+      <div>
+        <h2 className=" p-4 mx-6 hover:text-yellow-500"><a href="/">Restaurant</a></h2>
+      </div>
+      <div>
+        <ul className=" flex">
+          <li className=" p-4 mx-6 hover:text-yellow-500"><a href="/">Home</a></li>
+          <li className=" p-4 mx-6"><a>Menu</a></li>
+          <li className=" p-4 mx-6"><a>Contact</a></li>
+        </ul>
+      </div>
+
+    </div>
+  )
+}
+
+export default NavBar
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+const Index = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1 className=" text-2xl">Home</h1>
+    </div>
+  )
+}
+
+export default Index
