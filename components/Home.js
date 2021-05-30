43b66f352519eb24b3656557434db81572f0d9e3
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-05-29 23:11:54 -0300

    update Menu responsive

--- components/Home.js
+++ components/Home.js
@@ -3,14 +3,14 @@ import React from 'react'
 const Home = () => {
   return (
     <div className=" h-screen bg-yellow-100">
-      <div className=" block flex-wrap justify-around p-20 sm:grid grid-cols-2 ">
+      <div className=" block flex-wrap justify-around p-10 sm:grid grid-cols-2 ">
         <div className=" p-10 ">
           <h1 className=' text-3xl font-semibold'>Restaurant Paris</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed erat ut sapien ullamcorper vulputate. Fusce sagittis eget ligula a fermentum. Sed tempus vulputate dui, placerat vulputate orci feugiat non.
           </p>
         </div>
         <div>
-          <div className=" h-full p-16 border-black border" >
+          <div className=" p-16 border-black border" >
             Image
           </div>
         </div>
--- components/Menu.js
+++ components/Menu.js
@@ -3,14 +3,14 @@ import React from 'react'
 const Menu = () => {
   return (
     <div className="h-screen bg-gray-600">
-      <div className=" block flex-wrap justify-around p-10 sm:grid grid-cols-3 gap-2 place-items-center">
-        <div className=' h-full bg-yellow-100 p-10 border border-black'>
+      <div className=" block flex-wrap justify-around p-10 sm:grid grid-cols-3 justify-items-center ">
+        <div className='bg-yellow-100 p-16 border border-black'>
           1
         </div>
-        <div className=' h-full bg-yellow-100 p-10 border border-black'>
+        <div className='bg-yellow-100 p-16 border border-black'>
           2
         </div>
-        <div className=' h-full bg-yellow-100 p-10 border border-black'>
+        <div className=' bg-yellow-100 p-16 border border-black'>
           3
         </div>
       </div>
