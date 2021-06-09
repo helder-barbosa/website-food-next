e67990879e56ef885db21204c81d01e680c10dba
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-06-08 22:49:03 -0300

    fix Comment width

--- components/Comments/Comments.js
+++ components/Comments/Comments.js
@@ -31,10 +31,10 @@ const Comments = () => {
       <div>
         <ul className='block flex-wrap justify-around p-10 sm:grid grid-cols-3 justify-items-center '>
           {CommentsData.map(item => (
-            <li className=' h-64 w-64 p-2 shadow-xl rounded-lg bg-gray-300 mb-2'>
+            <li key={item.id} className=' h-64 w-80 p-5 shadow-xl rounded-lg bg-gray-300 mb-3'>
               <div>
                 <div className=' h-32 overflow-hidden'>
-                  <img className=' block mx-auto w-1/2' src={item.image} alt={item.username} />
+                  <img className=' block mx-auto w-1/3' src={item.image} alt={item.username} />
                 </div>
                 <div className='text-center p-3'>
                   <h3 className=' text-xl font-bold'>{item.username}</h3>
